47f0d15c9e203f88d7a7745c1acdde3af516a2f2
Sidhaarth Shree <127885925+SidhaarthShree07@users.noreply.github.com>
2024-03-07 07:27:49 +0530

    Update models.py

--- myapp/models.py
+++ myapp/models.py
@@ -5,11 +5,11 @@ from tinymce import models as tinymce_models
 class Post(models.Model):  
     title = models.CharField(max_length=80)
     lab = models.CharField(max_length=80)
-    image = models.TextField()
+    image = models.CharField(max_length=500)
     link = models.CharField(max_length=80)
     desc = models.TextField()
     proc = tinymce_models.HTMLField()
-    show = models.ImageField(upload_to='images/', default='images/default.png')
+    show = models.CharField(max_length=500)
     
     def __str__(self):
         return self.title
